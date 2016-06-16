ae37a3351651f93fa86ed50094d9d2b136b6a504
Robin Haberkorn <haberkorn@metratec.com>
2016-06-16 20:13:39 +0200

    license cleanup
     * the project appears to be licensed under the LGPLv2+ according to the
       license headers in most of the source files and the old project page
       at https://code.google.com/archive/p/gst-editor/.
     * still the COPYING file was pointing to the GPLv2. This is now fixed.
     * Missing source code header licenses have been added.
       Copyright has been attributed to Erik Walthinsen, for lack of
       better information on who created them initially.
     * Robin Haberkorn should perhaps be added to most of the copyrights as well!??
     * The about dialog contains the short LGPLv2+ note now as well.

--- libs/gst/editor/gsteditor.c
+++ libs/gst/editor/gsteditor.c
@@ -888,6 +888,22 @@ gst_editor_on_about (GtkWidget * widget, GstEditor * editor)
       "version", VERSION,
       "copyright",
       "(c) 2001-2007 GStreamer Team, 2008-2010 Hannes Bistry, 2016 metraTec GmbH",
+      "license",
+      "This library is free software; you can redistribute it and/or "
+      "modify it under the terms of the GNU Library General Public "
+      "License as published by the Free Software Foundation; either "
+      "version 2 of the License, or (at your option) any later version.\n"
+      "\n"
+      "This library is distributed in the hope that it will be useful, "
+      "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU "
+      "Library General Public License for more details.\n"
+      "\n"
+      "You should have received a copy of the GNU Library General Public "
+      "License along with this library; if not, write to the "
+      "Free Software Foundation, Inc., 59 Temple Place - Suite 330, "
+      "Boston, MA 02111-1307, USA.",
+      "wrap-license", TRUE,
       "comments",
       "A graphical pipeline editor for "
       "GStreamer capable of loading and saving XML.",
--- libs/gst/editor/gsteditorlink.c
+++ libs/gst/editor/gsteditorlink.c
@@ -1,6 +1,30 @@
+/* GStreamer
+ * Copyright (C) <1999> Erik Walthinsen <omega@cse.ogi.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include "config.h"
+
+#include <gst/gst.h>
+#include <goocanvas.h>
+
+#include <gst/common/gste-debug.h>
+
 #include "editor.h"
 #include "gst-helper.h"
-#include <gst/common/gste-debug.h>
 
 /* class functions */
 static void gst_editor_link_class_init (GstEditorLinkClass * klass);
