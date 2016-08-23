metratec/gst-editor

commit e98d65634510befc867692fb89efe1a4a31915db

selection an element updates the PRIMARY selection/clipboard
 * gst_editor_bin_paste() and gst_editor_element_copy() have been updated
   to accept arbitrary X11 selection atoms.
 * updating the PRIMARY clipboard on selection is how the X11 clipboard
   should be used. Every selected element can be immediately pasted e.g.
   into an editor; but pasting it into the GstEditor requires you to copy it
   to the CLIPBOARD selection (Copy/Cut in menu) first.
 * the serialization on select can be optimized: By simply calling
   gtk_clipboard_set_with_data() when the selection changes and
   serializing only on demand. However, serialization is probably not
   a performance issue.

libs/gst/editor/gsteditorcanvas.c
@@ -274,6 +274,14 @@ gst_editor_canvas_set_property (GObject * object, guint prop_id,
         g_object_set (canvas->property_window, "element", NULL, NULL);
       }
 
+      /*
+       * Update the PRIMARY selection clipboard.
+       * TODO: Instead of always serializing the selected element, it is
+       * possible to do this on-demand only since this selection property
+       * is always kept up to date.
+       * See gtk_clipboard_set_with_data().
+       */
+      gst_editor_element_copy (canvas->selection, GDK_SELECTION_PRIMARY);
       break;
 
     case PROP_PROPERTIES_VISIBLE:

libs/gst/editor/gsteditor.c
@@ -743,7 +743,7 @@ gst_editor_on_cut (GtkWidget * widget, GstEditor * editor)
     return;
   }
 
-  gst_editor_element_cut (element);
+  gst_editor_element_cut (element, GDK_SELECTION_CLIPBOARD);
 }
 
 void
@@ -759,13 +759,13 @@ gst_editor_on_copy (GtkWidget * widget, GstEditor * editor)
     return;
   }
 
-  gst_editor_element_copy (element);
+  gst_editor_element_copy (element, GDK_SELECTION_CLIPBOARD);
 }
 
 void
 gst_editor_on_paste (GtkWidget * widget, GstEditor * editor)
 {
-  gst_editor_bin_paste (editor->canvas->bin);
+  gst_editor_bin_paste (editor->canvas->bin, GDK_SELECTION_CLIPBOARD);
 }
 
 void

libs/gst/editor/gsteditorbin.c
@@ -577,7 +577,7 @@ on_paste (GSimpleAction * action,
 {
   GstEditorBin *bin = GST_EDITOR_BIN (user_data);
 
-  gst_editor_bin_paste (bin);
+  gst_editor_bin_paste (bin, GDK_SELECTION_CLIPBOARD);
 }
 
 /**********************************************************************
@@ -761,9 +761,9 @@ gst_editor_bin_paste_from_string (GstEditorBin * bin, const gchar * str,
 }
 
 void
-gst_editor_bin_paste (GstEditorBin * bin)
+gst_editor_bin_paste (GstEditorBin * bin, GdkAtom selection)
 {
-  GtkClipboard *clipboard = gtk_clipboard_get (GDK_NONE);
+  GtkClipboard *clipboard = gtk_clipboard_get (selection);
   gchar *text;
   GError *error = NULL;
 

libs/gst/editor/gsteditorbin.h
@@ -60,7 +60,7 @@ GType gst_editor_bin_get_type (void);
 gdouble gst_editor_bin_sort (GstEditorBin * bin, gdouble step);
 gboolean gst_editor_bin_paste_from_string (GstEditorBin * bin,
     const gchar * str, GError ** error);
-void gst_editor_bin_paste (GstEditorBin * bin);
+void gst_editor_bin_paste (GstEditorBin * bin, GdkAtom selection);
 void gst_editor_bin_debug_output (GstEditorBin * bin);
 
 /*

libs/gst/editor/gsteditorelement.c
@@ -1455,7 +1455,7 @@ on_copy (GSimpleAction * action,
 {
   GstEditorElement * element = GST_EDITOR_ELEMENT (user_data);
 
-  gst_editor_element_copy (element);
+  gst_editor_element_copy (element, GDK_SELECTION_CLIPBOARD);
 }
 
 static void
@@ -1464,7 +1464,7 @@ on_cut (GSimpleAction * action,
 {
   GstEditorElement * element = GST_EDITOR_ELEMENT (user_data);
 
-  gst_editor_element_cut (element);
+  gst_editor_element_cut (element, GDK_SELECTION_CLIPBOARD);
 }
 
 static void
@@ -1564,16 +1564,16 @@ gst_editor_element_move (GstEditorElement * element, gdouble dx, gdouble dy)
 }
 
 void
-gst_editor_element_cut (GstEditorElement * element)
+gst_editor_element_cut (GstEditorElement * element, GdkAtom selection)
 {
-  gst_editor_element_copy (element);
+  gst_editor_element_copy (element, selection);
   gst_editor_element_remove (element);
 }
 
 void
-gst_editor_element_copy (GstEditorElement * element)
+gst_editor_element_copy (GstEditorElement * element, GdkAtom selection)
 {
-  GtkClipboard *clipboard = gtk_clipboard_get (GDK_NONE);
+  GtkClipboard *clipboard = gtk_clipboard_get (selection);
   gchar *buffer;
 
   /*

libs/gst/editor/gsteditorelement.h
@@ -103,8 +103,9 @@ struct _GstEditorElementClass
 GType gst_editor_element_get_type (void);
 void gst_editor_element_move (GstEditorElement * element,
     gdouble dx, gdouble dy);
-void gst_editor_element_cut (GstEditorElement * element);
-void gst_editor_element_copy (GstEditorElement * element);
+
+void gst_editor_element_cut (GstEditorElement * element, GdkAtom selection);
+void gst_editor_element_copy (GstEditorElement * element, GdkAtom selection);
 void gst_editor_element_remove (GstEditorElement * element);
 
 void gst_editor_element_stop_child (GstEditorElement * child);

commit ff68bc5103c4e625e1587aca7109d899c4cfbc40

fixed unsetting canvas selection
 * the clipboard can only be updated when a selection is set
 * when it is unset, we should clear the clipboard
 * this bug was introduced in e98d65634510befc867692fb89efe1a4a31915db

libs/gst/editor/gsteditorcanvas.c
@@ -294,18 +294,24 @@ gst_editor_canvas_set_property (GObject * object, guint prop_id,
         if (canvas->property_window)
           g_object_set (canvas->property_window, "element",
               GST_EDITOR_ITEM (canvas->selection)->object, NULL);
-      } else if (canvas->property_window) {
-        g_object_set (canvas->property_window, "element", NULL, NULL);
-      }
 
-      /*
-       * Update the PRIMARY selection clipboard.
-       * TODO: Instead of always serializing the selected element, it is
-       * possible to do this on-demand only since this selection property
-       * is always kept up to date.
-       * See gtk_clipboard_set_with_data().
-       */
-      gst_editor_element_copy (canvas->selection, GDK_SELECTION_PRIMARY);
+        /*
+         * Update the PRIMARY selection clipboard.
+         * TODO: Instead of always serializing the selected element, it is
+         * possible to do this on-demand only since this selection property
+         * is always kept up to date.
+         * See gtk_clipboard_set_with_data().
+         */
+        gst_editor_element_copy (canvas->selection, GDK_SELECTION_PRIMARY);
+      } else {
+        if (canvas->property_window)
+          g_object_set (canvas->property_window, "element", NULL, NULL);
+
+        /*
+         * Clear the PRIMARY selection clipboard.
+         */
+        gtk_clipboard_clear (gtk_clipboard_get (GDK_SELECTION_PRIMARY));
+      }
       break;
 
     case PROP_PROPERTIES_VISIBLE: